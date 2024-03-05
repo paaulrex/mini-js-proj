a660962e197a6aede11139a1fd4821d971a87131
Paul De Silos <pauldesilos@gmail.com>
2024-03-05 15:18:25 -0600

    first commit

---
+++ destructuring/script.js
@@ -0,0 +1,11 @@
+const randomThings = {
+    diveLocation: 'Guam',
+    activity: 'scuba diving',
+    diveSpot: 'Gab Gab 2',
+    pointOfInterets: 'nurse shark',
+    company: 'diving friends'
+}
+
+const {diveLocation, activity, diveSpot, pointOfInterets, company} = randomThings
+
+console.log(`I would like to go back to ${diveLocation} to go ${activity} at ${diveSpot} to see a ${pointOfInterets} with my ${company}`)
